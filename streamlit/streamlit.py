1d44082630f528fbcce6830103abb8b4ac12152b
Archymen <122228956+Archymen@users.noreply.github.com>
2023-08-30 23:16:15 +0300

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -1,9 +1,11 @@
+!pip3 install yfinance
 import streamlit as st
 import pandas as pd
 import numpy as np
 import yfinance as yf
 import datetime
 import plotly.express as px
+
 
 st.set_page_config(page_title='Doc', page_icon=':bar_chart:', layout='wide')
 
